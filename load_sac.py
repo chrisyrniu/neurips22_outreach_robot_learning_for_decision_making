74ecaccae98d8f13d4357e2a441898ae3ae53def
chrisyrniu <chrisyrniu@gmail.com>
2022-11-25 05:44:22 -0800

    updae

--- load_sac.py
+++ load_sac.py
@@ -7,7 +7,6 @@ import pprint
 
 import numpy as np
 import torch
-from mujoco_env import make_mujoco_env
 from torch.utils.tensorboard import SummaryWriter
 
 from tianshou.data import Collector, ReplayBuffer, VectorReplayBuffer, Batch, to_numpy
@@ -21,54 +20,51 @@ import gym
 
 
 def load_sac(resume_path, env, device='cuda'):
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()
-    args.device = device
-    args.actor_lr = 0.001
-    args.critic_lr = 0.001
-    args.hidden_sizes = [256, 256]
-    args.resume_path = resume_path
+    actor_lr = 0.001
+    critic_lr = 0.001
+    hidden_sizes = [256, 256]
+    resume_path = resume_path
 
     env, train_envs, test_envs = make_mujoco_env(
         env, 0, 1, 1, obs_norm=False
     )
 
-    args.state_shape = env.observation_space.shape or env.observation_space.n
-    args.action_shape = env.action_space.shape or env.action_space.n
-    args.max_action = env.action_space.high[0]
-    args.seed = 0
+    state_shape = env.observation_space.shape or env.observation_space.n
+    action_shape = env.action_space.shape or env.action_space.n
+    max_action = env.action_space.high[0]
+    seed = 0
     # seed
-    np.random.seed(args.seed)
-    torch.manual_seed(args.seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
     # model
-    net_a = Net(args.state_shape, hidden_sizes=args.hidden_sizes, device=args.device)
+    net_a = Net(state_shape, hidden_sizes=hidden_sizes, device=device)
     actor = ActorProb(
         net_a,
-        args.action_shape,
-        max_action=args.max_action,
-        device=args.device,
+        action_shape,
+        max_action=max_action,
+        device=device,
         unbounded=True,
         conditioned_sigma=True,
-    ).to(args.device)
-    actor_optim = torch.optim.Adam(actor.parameters(), lr=args.actor_lr)
+    ).to(device)
+    actor_optim = torch.optim.Adam(actor.parameters(), lr=actor_lr)
     net_c1 = Net(
-        args.state_shape,
-        args.action_shape,
-        hidden_sizes=args.hidden_sizes,
+        state_shape,
+        action_shape,
+        hidden_sizes=hidden_sizes,
         concat=True,
-        device=args.device,
+        device=device,
     )
     net_c2 = Net(
-        args.state_shape,
-        args.action_shape,
-        hidden_sizes=args.hidden_sizes,
+        state_shape,
+        action_shape,
+        hidden_sizes=hidden_sizes,
         concat=True,
-        device=args.device,
+        device=device,
     )
-    critic1 = Critic(net_c1, device=args.device).to(args.device)
-    critic1_optim = torch.optim.Adam(critic1.parameters(), lr=args.critic_lr)
-    critic2 = Critic(net_c2, device=args.device).to(args.device)
-    critic2_optim = torch.optim.Adam(critic2.parameters(), lr=args.critic_lr)
+    critic1 = Critic(net_c1, device=device).to(device)
+    critic1_optim = torch.optim.Adam(critic1.parameters(), lr=critic_lr)
+    critic2 = Critic(net_c2, device=device).to(device)
+    critic2_optim = torch.optim.Adam(critic2.parameters(), lr=critic_lr)
 
     policy = SACPolicy(
         actor,
@@ -79,7 +75,7 @@ def load_sac(resume_path, env, device='cuda'):
         critic2_optim,
     )
 
-    policy.load_state_dict(torch.load(args.resume_path, map_location=args.device))
-    print("Loaded agent from: ", args.resume_path)
+    policy.load_state_dict(torch.load(resume_path, map_location=device))
+    print("Loaded agent from: ", resume_path)
 
     return policy
